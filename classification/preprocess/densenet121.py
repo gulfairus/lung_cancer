02cd0c94b9e70252942627885e249ab68d0ce3a7
Gulfairus Kairedenova <gulfairuskairedenova@gmail.com>
2025-05-22 14:59:18 +0500

    extract features 001

--- classification/preprocess/densenet121.py
+++ classification/preprocess/densenet121.py
@@ -12,12 +12,13 @@ from google.cloud import storage
 
 from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input
 
-main_dir = "/content/gdrive/My Drive/data/lung_cancer/"
-main_dir_cloud= "/home/gulfairus/.database/lung_cancer/data/"
+#main_dir = "/content/gdrive/My Drive/data/lung_cancer/"
+#main_dir_cloud= "/home/gulfairus/.database/lung_cancer/data/"
 #tar = tarfile.open(os.path.join(main_dir_cloud, "images_001.tar.gz"))
-model = DenseNet121(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
+#model = DenseNet121(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
 
 def extract_features_001():
+    main_dir_cloud= "/home/gulfairus/.database/lung_cancer/data/"
     model = DenseNet121(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
     tar = tarfile.open(os.path.join(main_dir_cloud, "raw", "images_001.tar.gz"))
     features = {}
@@ -35,7 +36,7 @@ def extract_features_001():
             # print(image.shape)
             feature = model.predict(image)
             features[img] = feature
-            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            #timestamp = time.strftime("%Y%m%d-%H%M%S")
 
     feature_path = os.path.join(main_dir_cloud, "processed/features/densenet121", "images_001.pickle")
     with open(feature_path, "wb") as file:
