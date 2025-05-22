fb549b66bb4b7f3551d40dae875bb04a1cbfe3b9
Gulfairus Kairedenova <gulfairuskairedenova@gmail.com>
2025-05-22 15:16:04 +0500

    extract features 001

--- classification/preprocess/densenet121.py
+++ classification/preprocess/densenet121.py
@@ -17,28 +17,35 @@ from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input
 #tar = tarfile.open(os.path.join(main_dir_cloud, "images_001.tar.gz"))
 #model = DenseNet121(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
 
-def extract_features_001():
-    main_dir_cloud= "/home/gulfairus/.database/lung_cancer/data/"
-    model = DenseNet121(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
-    tar = tarfile.open(os.path.join(main_dir_cloud, "raw", "images_001.tar.gz"))
-    features = {}
-    for member in tar.getmembers():
-        if len(member.name.split('/'))==2:
-            img = member.name.split('/')[1]
-            image1 = Image.open(tar.extractfile(member))
-            image2 = image1.convert('RGB')
-            image3 = image2.resize((224,224))
-            image4 = np.expand_dims(image3, axis=0)
-            image5 = preprocess_input(image4)
-            image6 = image5/255
-        #    image = image - 1.0
-            # image = image.flatten()
-            # print(image.shape)
-            feature = model.predict(image6)
-            features[img] = feature
-            #timestamp = time.strftime("%Y%m%d-%H%M%S")
+def preprocess_features_001():
+    def extract_features():
+        main_dir_cloud= "/home/gulfairus/.database/lung_cancer/data/"
+        model = DenseNet121(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
+        tar = tarfile.open(os.path.join(main_dir_cloud, "raw", "images_001.tar.gz"))
+        features = {}
+        for member in tar.getmembers():
+            if len(member.name.split('/'))==2:
+                img = member.name.split('/')[1]
+                image1 = Image.open(tar.extractfile(member))
+                image2 = image1.convert('RGB')
+                image3 = image2.resize((224,224))
+                image4 = np.expand_dims(image3, axis=0)
+                image5 = preprocess_input(image4)
+                image6 = image5/255
+            #    image = image - 1.0
+                # image = image.flatten()
+                # print(image.shape)
+                feature = model.predict(image6)
+                features[img] = feature
+                #timestamp = time.strftime("%Y%m%d-%H%M%S")
+                print("✅ Features extracted")
+        return features
 
-    feature_path = os.path.join(main_dir_cloud, "processed/features/densenet121", "images_001.pickle")
-    with open(feature_path, "wb") as file:
-        pickle.dump(features, file)
-    return features
+    def save_results(features: dict) -> None:
+        if features is not None:
+            feature_path = os.path.join(main_dir_cloud, "processed/features/densenet121", "images_001.pickle")
+            with open(feature_path, "wb") as file:
+                pickle.dump(features, file)
+
+        print("✅ Features saved")
+    return None
