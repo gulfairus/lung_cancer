7b1f7152854b08c60eaabbd345c2fac1581b59a7
Gulfairus Kairedenova <gulfairuskairedenova@gmail.com>
2025-06-26 19:11:31 +0500

    generator

--- classification/resnet50/ml_logic/preprocessor_local.py
+++ classification/resnet50/ml_logic/preprocessor_local.py
@@ -66,10 +66,12 @@ def preprocess_data():
         arr = (arr - np.min(arr)) / (np.max(arr) - np.min(arr))  # Normalize to [0,1]
         arr = np.stack([arr] * 3, axis=-1)  # Make 3-channel RGB
         mean = np.mean(arr)
+        print(mean.shape)
         std = np.std(arr) + 1e-6  # add epsilon to avoid div by 0
+        print(std.shape)
         arr = (arr - mean) / std
-        return arr.astype(np.float32), np.array(mean, dtype=np.float32), np.array(std, dtype=np.float32)
-        #return np.mean(arr), np.std(arr)
+        #return arr.astype(np.float32), np.array(mean, dtype=np.float32), np.array(std, dtype=np.float32)
+        return arr, mean, std
 
 
 
