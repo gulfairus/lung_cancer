ff0182fc1caaab227debc86f43a9b7c02494dc83
Gulfairus Kairedenova <gulfairuskairedenova@gmail.com>
2025-06-30 19:40:27 +0500

    mean

--- classification/resnet50/ml_logic/preprocessor_train.py
+++ classification/resnet50/ml_logic/preprocessor_train.py
@@ -154,7 +154,7 @@ def train_data():
     dataset = tf.data.Dataset.from_tensor_slices((dicom_paths, label_tensor))
     dataset = dataset.map(read_dicom_from_gcs2, num_parallel_calls=tf.data.AUTOTUNE)
     #ds_for_training = dataset.map(lambda x, y: (x, y['label']))
-    dataset = dataset.shuffle(1000).batch(32).prefetch(tf.data.AUTOTUNE)
+    dataset = dataset.shuffle(100).batch(32).prefetch(tf.data.AUTOTUNE)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
