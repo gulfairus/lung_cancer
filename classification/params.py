53cec8f450f139bb0e3aac695c39db30d8a80b41
Gulfairus Kairedenova <gulfairuskairedenova@gmail.com>
2025-07-15 09:38:37 +0500

    train

--- classification/params.py
+++ classification/params.py
@@ -3,7 +3,7 @@ import numpy as np
 
 ##################  VARIABLES  ##################
 DATA_SIZE = os.environ.get("DATA_SIZE")
-CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE"))
+CHUNK_SIZE = os.environ.get("CHUNK_SIZE")
 MODEL_TARGET = os.environ.get("MODEL_TARGET")
 GCP_PROJECT = os.environ.get("GCP_PROJECT")
 GCP_PROJECT_WAGON = os.environ.get("GCP_PROJECT_WAGON")
